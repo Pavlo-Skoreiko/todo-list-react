d82c8e853be8e3311146d38a61cfe7b717355bf5
Pavlo Skoreiko <pavelroberts@gmail.com>
2023-01-04 19:43:05 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -28,8 +28,7 @@ function App() {
           return { ...task, done: !task.done };
         }
         return task;
-      })
-    );
+      }));
   };
 
   const setAllDone = () => {
@@ -37,14 +36,26 @@ function App() {
       tasks.map((task) => ({
         ...task,
         done: true,
-      }))
-    );
+      })));
+  };
+
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
   };
 
   return (
     <Container className="container">
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section title="Dodaj nowe zadanie"
+      body={<Form addNewTask={addNewTask} />}
+      />
 
       <Section
         title="Lista zadań"
