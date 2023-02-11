1c2a0831a6574ffde1ccc7714085a13cd40c1227
Pavlo Skoreiko <pavelroberts@gmail.com>
2023-02-11 16:38:11 +0100

    install react-router-dom

--- src/test.js
+++
@@ -1,7 +0,0 @@
-// функция для проверки является ли строка панграммой, если да то вернуть true, если нет то false
-function isPangram(str) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const strArr = str.toLowerCase().split('');
-  const result = alphabet.split('').every((letter) => strArr.includes(letter));
-  return result;
-}
